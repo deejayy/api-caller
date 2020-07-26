818b056bb3c353e2fec531610a43539528bfd29f
deejayy <deejayy@deejayy.hu>
2020-07-27 00:13:03 +0200

    Adjust compiler options

--- projects/api-caller/src/index.ts
+++ projects/api-caller/src/index.ts
@@ -1,2 +1,5 @@
 export * from './lib/service/api-caller.service';
 export * from './lib/api-caller.module';
+export * from './lib/model/api-call-item.model';
+export * from './lib/store/api.state';
+export * from './lib/service/api-connector';
