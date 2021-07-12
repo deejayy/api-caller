01371deebc9b2c40d8ae731cbeff695967d8ff1b
deejayy <deejayy@deejayy.hu>
2021-07-12 16:01:55 +0200

    Angular v11 compat

--- projects/api-caller/src/lib/helper/produce-on.ts
+++ projects/api-caller/src/lib/helper/produce-on.ts
@@ -1,12 +1,13 @@
 import { Draft, produce, enableMapSet } from 'immer';
-import { ActionCreator, ActionType, on, On } from '@ngrx/store';
+import { ActionCreator, ActionType, on } from '@ngrx/store';
 
 enableMapSet();
 
 export const produceOn = <C1 extends ActionCreator, S>(
   actionType: C1,
   callback: (draft: Draft<S>, action: ActionType<C1>) => void,
-): On<S> => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+): any => {
   return on(
     actionType,
     (state: S, action: ActionType<C1>): S => produce(state, (draft: Draft<S>) => callback(draft, action)),
